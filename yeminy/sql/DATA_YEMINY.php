aebfa56ff8f97162bbb3e7f5b24d9c38c5b43dbd
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2015-12-24 03:32:09 +0100

    included texts in setup

--- yeminy/sql/DATA_YEMINY.php
+++ yeminy/sql/DATA_YEMINY.php
@@ -4,6 +4,7 @@ class DATA_YEMINY extends \SYSTEM\DB\QI {
     public static function get_class(){return \get_class();}
     public static function files_mysql(){
         return array(   \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_api.sql'),
-                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_page.sql'));
+                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_page.sql'),
+                        \SYSTEM\SERVERPATH(new \PSQL(),'/mysql/system_text.sql'));
     }    
 }
